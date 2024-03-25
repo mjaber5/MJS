780d3dd0d091aa0e895d4fe892a3d74dfca531a1
mo7md27 <mohammadabushera77@gmail.com>
2024-03-25 14:46:07 +0300

    i build post screen

---
+++ lib/models/post.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PostModel {
+  String userId;
+  String email;
+  String displayName;
+  String userName;
+  String bio;
+  String profilePicture;
+  List followers;
+  List following;
+
+  PostModel({
+    required this.userId,
+    required this.email,
+    required this.displayName,
+    required this.userName,
+    required this.bio,
+    required this.profilePicture,
+    required this.followers,
+    required this.following,
+  });
+
+  factory PostModel.fromSnap(DocumentSnapshot snap) {
+    var snapShot = snap.data() as Map<String, dynamic>;
+    return PostModel(
+      userId: snapShot['userId'],
+      email: snapShot['email'],
+      displayName: snapShot['displayName'],
+      userName: snapShot['userName'],
+      bio: snapShot['bio'],
+      profilePicture: snapShot['profilePicture'],
+      followers: snapShot['followers'],
+      following: snapShot['following'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "email": email,
+        "displayName": displayName,
+        "userName": userName,
+        "bio": bio,
+        "profilePicture": profilePicture,
+        "followers": followers,
+        "following": following,
+      };
+}
--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:social_media_project/colors/app_color.dart';
 
 class AddPage extends StatefulWidget {
   const AddPage({super.key});
@@ -11,10 +14,46 @@ class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'AddPage',
-          style: Theme.of(context).textTheme.titleLarge,
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+        title: const Text("Add Post"),
+        actions: [TextButton(onPressed: () {}, child: const Text("Post"))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/man.png'),
+                ),
+                Gap(30),
+                Expanded(
+                  child: TextField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Type here.....",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  backgroundColor: kSecondaryColor,
+                  padding: const EdgeInsets.all(20)),
+              onPressed: () {},
+              child: Icon(
+                Ionicons.camera_outline,
+                color: kWhiteColor,
+              ),
+            ),
+            const Gap(80),
+          ],
         ),
       ),
     );
